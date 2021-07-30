c3784a172c0c3e0a689a0fb3888be5c0c8a882f2
Denis-s4n <denis.nt.nc@gmail.com>
2021-07-30 12:09:45 -0300

    Project created

---
+++ src/main/java/com/san/webServiceCourse/WebServiceCourseApplication.java
@@ -0,0 +1,13 @@
+package com.san.webServiceCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/san/webServiceCourse/WebServiceCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.webServiceCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
