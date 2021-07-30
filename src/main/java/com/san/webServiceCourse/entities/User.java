2505f7d6ff276c57f928fb115fa284e3fce5a94d
Denis-s4n <denis.nt.nc@gmail.com>
2021-07-30 20:03:29 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/san/webServiceCourse/entities/User.java
+++ src/main/java/com/san/webServiceCourse/entities/User.java
@@ -1,17 +1,22 @@
 package com.san.webServiceCourse.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
-public class User implements Serializable{
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -19,12 +24,13 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
-	
-	public User() {
-		
-	}
 
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<Order>();
+
+	public User() {
+
+	}
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -35,62 +41,54 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, id, name);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -103,6 +101,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
-	
-	
+
 }
