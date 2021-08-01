e8814852b607070c305469673b800b9da9171aee
Denis-s4n <denis.nt.nc@gmail.com>
2021-08-01 09:07:08 -0300

    Product entity

--- src/main/java/com/san/webServiceCourse/resources/CategoryResource.java
+++ src/main/java/com/san/webServiceCourse/resources/CategoryResource.java
@@ -2,10 +2,7 @@ package com.san.webServiceCourse.resources;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ src/main/java/com/san/webServiceCourse/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.san.webServiceCourse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.san.webServiceCourse.entities.Product;
+import com.san.webServiceCourse.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> optional = repository.findById(id);
+		return optional.get();
+	}
+}
