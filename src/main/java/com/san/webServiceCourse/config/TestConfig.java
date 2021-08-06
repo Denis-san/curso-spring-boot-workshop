d547582f64efc0eb27a48b613ae10c7a4a1811bf
Denis-s4n <denis.nt.nc@gmail.com>
2021-08-06 14:51:27 -0300

    Payment, one-to-one association

--- src/main/java/com/san/webServiceCourse/config/TestConfig.java
+++ src/main/java/com/san/webServiceCourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.san.webServiceCourse.entities.Category;
 import com.san.webServiceCourse.entities.Order;
 import com.san.webServiceCourse.entities.OrderItem;
+import com.san.webServiceCourse.entities.Payment;
 import com.san.webServiceCourse.entities.Product;
 import com.san.webServiceCourse.entities.User;
 import com.san.webServiceCourse.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
